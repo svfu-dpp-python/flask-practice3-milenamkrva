f836d7c747207e3f3a2be027af8d0f4005b27f18
MilenaMakarova <m.makarova75@gmail.com>
2024-04-16 22:46:12 +0900

    Добавление моделей SqlAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from app import views
+from app.database import db, migrate
+
 def create_app():
     app = Flask(__name__)
 
     app.add_url_rule("/", view_func=views.index_page)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     return app
